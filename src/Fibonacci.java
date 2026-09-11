344967af5815e90b0c272b8ed266737f2ccc8bd3
Aman Kumar <aman2007kumar2007@gmail.com>
2026-09-11 09:42:51 +0530

    fibonacci

---
+++ src/Fibonacci.java
@@ -0,0 +1,20 @@
+//To calculate Fibonacci Series up to n numbers.
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number where up to find Fibonacci Series: ");
+        int n = sc.nextInt();
+        int a = 0, b = 1;
+        for(int i = 1; i <= n; i++) {
+            int ans = a + b;
+            System.out.print(a+" ");
+            a = b;
+            b = ans;
+        }
+    }
+}
+
+//Fibonacci Series: 0 1 1 2 3 5 8 13 21 34
