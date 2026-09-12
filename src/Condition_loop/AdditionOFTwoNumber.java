ff863dc3cb2c9962349b7186cb635585ba09585f
Aman Kumar <aman2007kumar2007@gmail.com>
2026-09-12 14:11:12 +0530

    Addition Of Two Numbers

---
+++ src/Condition_loop/AdditionOFTwoNumber.java
@@ -0,0 +1,14 @@
+package Condition_loop;
+
+import java.util.Scanner;
+
+public class AdditionOFTwoNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 1st number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter 2nd number: ");
+        int b = sc.nextInt();
+        System.out.println("Sum: "+(a+b));
+    }
+}
