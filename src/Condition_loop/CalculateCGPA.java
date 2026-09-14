47081f70fc77b8ce2db92370dfd0fb3042383f96
Aman Kumar <aman2007kumar2007@gmail.com>
2026-09-14 16:02:45 +0530

    Calculate CGPA Java Program

---
+++ src/Condition_loop/CalculateCGPA.java
@@ -0,0 +1,25 @@
+package Condition_loop;
+
+import java.util.Scanner;
+
+public class CalculateCGPA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 0 for stop input.");
+        double cgpa = 0;
+        int count = 0;
+        double sum = 0;
+        while(true) {
+            System.out.print("Enter Subject grade point: ");
+            double p = sc.nextDouble();
+            if(p == 0) {
+                cgpa = sum/count;
+                System.out.printf("The CGPA is %.2f", cgpa);
+                return;
+            }
+            sum += p;
+            count++;
+
+        }
+    }
+}
