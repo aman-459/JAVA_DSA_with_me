365b2f172110daf577294d1eb8c21b5b572b2d38
Aman Kumar <aman2007kumar2007@gmail.com>
2026-09-14 20:22:45 +0530

    sum on natural number 1 to n.

---
+++ src/Functions/SumNumber.java
@@ -0,0 +1,11 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class SumNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((n*(n+1))/2);
+    }
+}
