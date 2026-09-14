66adf2d95f761e46d40d1c35b85eccc5938cdda5
Aman Kumar <aman2007kumar2007@gmail.com>
2026-09-14 20:21:08 +0530

    factorial

---
+++ src/Functions/Factorial.java
@@ -0,0 +1,19 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if(n == 0 || n == 1) return 1;
+        int fact = 1;
+        for(int i = 2; i <= n; i++) fact *= i;
+        return fact;
+    }
+}
