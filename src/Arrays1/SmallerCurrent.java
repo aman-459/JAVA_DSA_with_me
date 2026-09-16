0f4e6ec3876fbe0a09316f435538fd3f733f2bdf
Aman Kumar <aman2007kumar2007@gmail.com>
2026-09-16 12:14:22 +0530

    How Many Numbers Are Smaller Than the Current Number

---
+++ src/Arrays1/SmallerCurrent.java
@@ -0,0 +1,23 @@
+package Arrays1;
+
+import java.util.Arrays;
+
+public class SmallerCurrent {
+    public static void main(String[] args) {
+        int[] arr = {8,1,2,2,3};
+        System.out.println(Arrays.toString(smallerNumbersThanCurrent(arr)));
+
+    }
+
+    public static int[] smallerNumbersThanCurrent(int[] arr) {
+        int[] ans = new int[arr.length];
+        for(int i = 0; i < arr.length; i++) {
+            int count = 0;
+            for(int j = 0; j < arr.length; j++) {
+                if(arr[i] > arr[j]) count++;
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+}
